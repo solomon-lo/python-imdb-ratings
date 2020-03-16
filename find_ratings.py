bc81333ea19588690326f6cfbb68c1190637b6f4
Solomon Lo <losolomon0@gmail.com>
2020-03-16 09:12:55 +0800

    get website to search from

--- find_ratings.py
+++ find_ratings.py
@@ -19,4 +19,17 @@ for film in filmswe:
     films.append(os.path.splitext(film)[0])
     print(os.path.splitext(film)[0])
 
-    
+for line in films:
+    x = line.split(", ")
+    title = line.lower()
+    release = x[1]
+    query = "+".join(title.split()) 
+    URL = "https://www.imdb.com/search/title/?title=" + query
+    print(URL)
+    # print(release)
+    try: 
+        response = s.get(URL)
+
+        #getting contect from IMDB Website
+        content = response.content
+        print(content)
