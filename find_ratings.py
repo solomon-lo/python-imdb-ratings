f3eb4ad37e9f4fafad0bb2e96c1eb995f567c753
Solomon Lo <losolomon0@gmail.com>
2020-06-15 15:55:34 +0800

    add data scraping

--- find_ratings.py
+++ find_ratings.py
@@ -23,20 +23,26 @@ for line in films:
     # x = line.split(", ")
     title = line.lower()
     # release = x[1]
-    query = "+".join(title.split()) 
+    query = "+".join(title.split())
     URL = "https://www.imdb.com/search/title/?title=" + query
     print(URL)
     # print(release)
-    try: 
+    try:
         response = s.get(URL)
-
         content = response.content
 
 
-        soup = BeautifulSoup(response.content, features="html.parser") 
+        soup = BeautifulSoup(response.content, features="html.parser")
+
         containers = soup.find_all("div", class_="lister-item-content")
         for result in containers:
             name1 = result.h3.a.text
-            print(name1)
             name = result.h3.a.text.lower()
-            print(name)
+
+            if title in name:
+                rating = result.find(
+                    "div", class_="inline-block ratings-imdb-rating")["data-value"]
+                genre = result.p.find("span", class_="genre")
+                genre = genre.contents[0]
+    except Exception:
+        print("Inputted title wasn't able to be found")
