9071c40ff64c044e37af324d86e6dab5fc3274aa
Solomon Lo <losolomon0@gmail.com>
2020-06-25 11:23:43 +0800

    output results to csv file

--- find_ratings.py
+++ find_ratings.py
@@ -3,32 +3,37 @@ import requests
 import pandas as pd
 import os
 
-
+#start a session
 s = requests.session()
 
+#arrays to hold data we will output to csv file
 films = []
 names = []
 ratings = []
 genres = []
 
+#get input path where we will automatically get names to search for
 path = input("Input the file directory where you store your movies: ")
 
 filmswe = os.listdir(path)
 
+#get each film's name by parsing file names
 for film in filmswe:
     films.append(os.path.splitext(film)[0])
     print(os.path.splitext(film)[0])
 
+#start data scraping
 for line in films:
     title = line.lower()
     query = "+".join(title.split())
+    #generate the correct url
     URL = "https://www.imdb.com/search/title/?title=" + query
     print(URL)
     try:
         response = s.get(URL)
         content = response.content
 
-
+        #start html parser for BeautifulSoup
         soup = BeautifulSoup(response.content, features="html.parser")
 
         containers = soup.find_all("div", class_="lister-item-content")
@@ -36,6 +41,7 @@ for line in films:
             name1 = result.h3.a.text
             name = result.h3.a.text.lower()
 
+            #get the titles needed
             if title in name:
                 rating = result.find(
                     "div", class_="inline-block ratings-imdb-rating")["data-value"]
